1891a60bb3471930be6323f44ee7abb005ac9502
jbradley84 <utbradley84@yahoo.com>
2022-07-02 16:26:27 -0500

    created employee constructor function test

---
+++ index.js
@@ -0,0 +1,23 @@
+// const inquirer = require('inquirer');
+
+// const promptTeam = () => {
+//    return inquirer.prompt ([
+//       {
+//          type: "input",
+//          name: "name",
+//          message: "Please enter TEAM MANAGER's name."
+//       },
+//       {
+//          type: "input",
+//          name: "id",
+//          message: "Please enter TEAM MANAGER's employee ID number."
+//       },
+//       {
+//          type: "input",
+//          name: "email",
+//          message: "Please enter TEAM MANAGER's company email address."
+//       }
+//    ])
+// };
+
+// promptTeam();
