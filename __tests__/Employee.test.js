85d645fb2887c3803a29b830eb9bc694a388f7da
jbradley84 <utbradley84@yahoo.com>
2022-07-02 17:07:14 -0500

    wrote test function getName

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -7,3 +7,14 @@ test('creates an employee object', () => {
    expect(employee.id).toEqual(expect.any(Number));
    expect(employee.email).toEqual(expect.any(String));
 });
+
+// getName
+test("gets employee's name as an object", () => {
+   const employee = new Employee();
+
+   expect(employee.getName()).toHaveProperty('name');
+});
+
+// getId
+// getEmail
+// getRole
